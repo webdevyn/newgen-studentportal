bb93aa320f1f43d49e9c173108b7c23659970813
Devyn Weir <devynweir@gmail.com>
2025-06-16 21:08:30 -0600

    Created Student List & Form

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#StudentList">Student List</a>
+        </li>
+        <li>
+          <a href="#AddStudent">Add Student</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ app/data/students.js
@@ -0,0 +1,34 @@
+const students = [
+  {
+    firstName: "Alice",
+    lastName: "Johnson",
+    dateOfBirth: "2006-04-12",
+    currentGrade: "A+",
+  },
+  {
+    firstName: "Brian",
+    lastName: "Smith",
+    dateOfBirth: "2005-11-23",
+    currentGrade: "B",
+  },
+  {
+    firstName: "Carla",
+    lastName: "Martinez",
+    dateOfBirth: "2007-02-05",
+    currentGrade: "C-",
+  },
+  {
+    firstName: "David",
+    lastName: "Lee",
+    dateOfBirth: "2006-08-19",
+    currentGrade: "B+",
+  },
+  {
+    firstName: "Eva",
+    lastName: "Nguyen",
+    dateOfBirth: "2005-06-30",
+    currentGrade: "A",
+  },
+];
+
+export default students;
