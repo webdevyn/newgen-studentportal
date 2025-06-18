737a024abb4d546674b57197ef2a2e8df3772407
Simranjit kaur gill <167651833+simrangill1547@users.noreply.github.com>
2025-06-17 18:24:30 -0600

    Update NavBar.jsx

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React from 'react';
 
-const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#StudentList">Student List</a>
-        </li>
-        <li>
-          <a href="#AddStudent">Add Student</a>
-        </li>
-      </ul>
-    </nav>
+/**
+ * Navbar Component
+ * 
+ * Name: Simranjit Kaur Gill
+ * Date: June 17, 2025
+ * Description:
+ * Renders a fixed navigation bar at the top of the page.
+ * Styled using Tailwind CSS with links to Home, Add Student, About, and Student List.
+ * This is a static and responsive UI component.
+ */
+
+function Navbar() {
+  return React.createElement(
+    'nav',
+    { className: 'bg-gray-800 p-4 fixed w-full' },
+    React.createElement(
+      'ul',
+      { className: 'flex space-x-6 justify-center' },
+      ['Home', 'Add Student', 'About', 'Student List'].map((text, index) =>
+        React.createElement(
+          'li',
+          {
+            key: index,
+            className: 'text-white hover:underline cursor-pointer px-4',
+          },
+          text
+        )
+      )
+    )
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
